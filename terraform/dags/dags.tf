603208ba5d5978a4316c3718eef7db9e2c05e8f4
Gabriel Chan <gabriel.chanyk@gmail.com>
2025-03-09 16:42:31 -0400

    add new tables

--- terraform/dags/dags.tf
+++ terraform/dags/dags.tf
@@ -1,8 +1,20 @@
 resource "google_storage_bucket_object" "composer_dags_py"{
     name   = "dags/transform/transform.py"
-    source = "dags/transform.py"
+    source = "dags/transform/transform.py"
     bucket = var.bucket
     metadata = {
       owner = var.owner
     }
+}
+
+resource "google_storage_bucket_object" "yaml_files" {
+  for_each = fileset("dags/transform/maps", "*.yaml")  # Discover all YAML files in the directory
+
+  name   = "dags/transform/maps/${each.value}"  # Preserve the same path in the bucket
+  source = "dags/transform/maps/${each.value}"  # Local path to the file
+  bucket = var.bucket                  # GCS bucket name
+
+  metadata = {
+    owner = var.owner
+  }
 }
--- terraform/terraform_config/terraform.tfvars
+++ terraform/terraform_config/terraform.tfvars
@@ -6,4 +6,5 @@ bucket = "northamerica-northeast1-com-19fd123f-bucket"
 bq_datasets = {
   "lnd"  = ["customers", "billing", "cellular_service_usage", "home_internet_usage"],
   "expl" = ["customers", "billing", "cellular_service_usage", "home_internet_usage"],
+  "curated" = ["customer_usage_bill", "internet_customer_city_count", "cellular_customer_city_count"],
 }
